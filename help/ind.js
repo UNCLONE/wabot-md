fbdaae5ab9467404b0ac38aa19bbdb5a11e4ce61
Akmalz <81015426+Dvnz99@users.noreply.github.com>
2021-11-30 21:30:43 +0700

    Update ind.js

--- help/ind.js
+++ help/ind.js
@@ -128,10 +128,8 @@ exports.listMenu = (time, date, salam, pushname, prefix, regUser, ping, latensi)
 ⬇️ *DOWNLOADER* 
 *${i+=1}.* ${prefix}tiktok *link*
 *${i+=1}.* ${prefix}play *query*
-*${i+=1}.* ${prefix}playmp4 *query*
 *${i+=1}.* ${prefix}ytmp3 *link*
 *${i+=1}.* ${prefix}ytmp4 *link*
-*${i+=1}.* ${prefix}ytdl *link*
 *${i+=1}.* ${prefix}facebook *link*
 *${i+=1}.* ${prefix}twitter *link*
 *${i+=1}.* ${prefix}instagram *link*
