aeb5fe3589224133ac3c10c6c3c1c171891f46ad
Akmall <81015426+Akmall-236@users.noreply.github.com>
2021-12-13 15:40:55 +0700

    Update index.js

--- message/index.js
+++ message/index.js
@@ -192,6 +192,19 @@ module.exports = async(ltzx, msg, m) => {
                                                                      
 const ping = moment().millisecond()
 
+const runtime = function(seconds) {
+	seconds = Number(seconds);
+	var d = Math.floor(seconds / (3600 * 24));
+	var h = Math.floor(seconds % (3600 * 24) / 3600);
+	var m = Math.floor(seconds % 3600 / 60);
+	var s = Math.floor(seconds % 60);
+	var dDisplay = d > 0 ? d + (d == 1 ? "day, " : "days, ") : "";
+	var hDisplay = h > 0 ? h + (h == 1 ? "hour, " : "hours, ") : "";
+	var mDisplay = m > 0 ? m + (m == 1 ? "minute, " : "minutes, ") : "";
+	var sDisplay = s > 0 ? s + (s == 1 ? "second" : "seconds") : "";
+	return dDisplay + hDisplay + mDisplay + sDisplay;
+}
+
 async function wikipedia(querry) {
   try {
     const link = await axios.get(`https://id.wikipedia.org/wiki/${querry}`)
@@ -267,7 +280,7 @@ async function wikipedia(querry) {
                 } catch {
                     var prof = 'https://i.ibb.co/Tq7d7TZ/age-hananta-495-photo.png'
                 }
-                sendFileFromUrl(from, prof, ind.listMenu(time, date, salam, pushname, prefix, regUser, ping, latensi), msg)
+                sendFileFromUrl(from, prof, ind.listMenu(time, date, salam, pushname, prefix, regUser, ping, latensi, runtime(process.uptime())), msg)
             }
             break
             // Owner
